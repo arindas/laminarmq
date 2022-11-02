7d834c4c3d1968cf7cd70fa8f2296e8b73bf5f59
Arindam Das <dasarindam.mails@gmail.com>
2022-11-02 16:55:07 +0530

    Added an in memory partition implementation for testing.

---
+++ src/server/partition/in_memory.rs
@@ -0,0 +1,195 @@
+use super::{Partition as BasePartition, Record, Request, Response};
+use async_trait::async_trait;
+use std::{collections::HashMap, error::Error, fmt::Display};
+
+pub struct Partition {
+    records: HashMap<u64, Record>,
+    size: usize,
+}
+
+impl Partition {
+    pub fn new() -> Self {
+        Self {
+            records: HashMap::new(),
+            size: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum PartitionError {
+    NotSupported,
+    NotFound,
+}
+
+impl Display for PartitionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PartitionError::NotSupported => write!(f, "Operation not supported."),
+            PartitionError::NotFound => write!(f, "Not found"),
+        }
+    }
+}
+
+impl Error for PartitionError {}
+
+#[async_trait(?Send)]
+impl BasePartition for Partition {
+    type Error = PartitionError;
+
+    async fn serve_idempotent(&self, request: Request) -> Result<Response, Self::Error> {
+        match request {
+            Request::RemoveExpired { expiry_duration: _ } => Err(PartitionError::NotSupported),
+            Request::LowestOffset => Ok(Response::LowestOffset(0)),
+            Request::HighestOffset => Ok(Response::HighestOffset(self.size as u64)),
+            Request::Append { record_bytes: _ } => Err(PartitionError::NotSupported),
+            Request::Read { offset } => self
+                .records
+                .get(&offset)
+                .cloned()
+                .map(|x| {
+                    let next_offset = x.offset + x.value.len() as u64;
+                    Response::Read {
+                        record: x,
+                        next_offset,
+                    }
+                })
+                .ok_or(PartitionError::NotFound),
+        }
+    }
+
+    async fn serve(&mut self, request: Request) -> Result<Response, Self::Error> {
+        match request {
+            Request::RemoveExpired { expiry_duration: _ } => Err(PartitionError::NotSupported),
+            Request::Append { record_bytes } => {
+                let current_offset = self.size as u64;
+                let record_size = record_bytes.len();
+                let record = Record {
+                    value: record_bytes.into(),
+                    offset: current_offset,
+                };
+
+                self.records.insert(current_offset, record);
+
+                self.size += record_size;
+
+                Ok(Response::Append {
+                    write_offset: current_offset,
+                })
+            }
+            _ => self.serve_idempotent(request).await,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Partition, PartitionError};
+    use crate::server::partition::{Partition as BasePartition, Request, Response};
+    use std::time::Duration;
+
+    #[test]
+    fn test_partition() {
+        futures_lite::future::block_on(async {
+            let mut partition = Partition::new();
+
+            assert!(matches!(
+                partition
+                    .serve_idempotent(Request::RemoveExpired {
+                        expiry_duration: Duration::from_secs(1)
+                    })
+                    .await,
+                Err(PartitionError::NotSupported)
+            ));
+
+            assert!(matches!(
+                partition
+                    .serve(Request::RemoveExpired {
+                        expiry_duration: Duration::from_secs(1)
+                    })
+                    .await,
+                Err(PartitionError::NotSupported)
+            ));
+
+            let records = [
+                "Lorem",
+                "ipsum",
+                "dolor",
+                "sit",
+                "amet,",
+                "consectetur",
+                "adipiscing",
+                "elit.",
+                "In",
+                "sagittis",
+                "orci",
+                "a",
+                "neque",
+                "aliquet,",
+                "a",
+                "rutrum",
+                "nisi",
+                "maximus.",
+            ];
+
+            let mut total_size = 0;
+
+            assert!(matches!(
+                partition
+                    .serve_idempotent(Request::Read { offset: 0 })
+                    .await,
+                Err(PartitionError::NotFound)
+            ));
+
+            for record in records {
+                if let Response::Append { write_offset } = partition
+                    .serve(Request::Append {
+                        record_bytes: record.as_bytes().to_vec(),
+                    })
+                    .await
+                    .unwrap()
+                {
+                    assert_eq!(write_offset, total_size);
+                } else {
+                    assert!(false, "Wrong response type!");
+                }
+
+                total_size += record.len() as u64;
+            }
+
+            if let Response::LowestOffset(lowest_offset) =
+                partition.serve(Request::LowestOffset).await.unwrap()
+            {
+                assert_eq!(lowest_offset, 0);
+            } else {
+                assert!(false, "Wrong response type!");
+            }
+
+            if let Response::HighestOffset(highest_offset) =
+                partition.serve(Request::HighestOffset).await.unwrap()
+            {
+                assert_eq!(highest_offset, total_size);
+            } else {
+                assert!(false, "Wrong response type!");
+            }
+
+            let mut offset = 0;
+
+            for i in 0..records.len() {
+                if let Response::Read {
+                    record,
+                    next_offset,
+                } = partition
+                    .serve_idempotent(Request::Read { offset })
+                    .await
+                    .unwrap()
+                {
+                    assert_eq!(record.value, records[i].as_bytes());
+                    offset = next_offset;
+                } else {
+                    assert!(false, "Wrong response type!");
+                }
+            }
+        });
+    }
+}
--- src/server/partition/mod.rs
+++ src/server/partition/mod.rs
@@ -14,25 +14,39 @@ pub struct PartitionId {
 }
 
 pub enum Request {
-    Append { record_bytes: Vec<u8> },
-    Read { offset: u64 },
+    RemoveExpired {
+        expiry_duration: std::time::Duration,
+    },
+    LowestOffset,
+    HighestOffset,
+    Append {
+        record_bytes: Vec<u8>,
+    },
+    Read {
+        offset: u64,
+    },
 }
 
 impl Request {
     pub fn idempotent(&self) -> bool {
         match self {
+            Request::RemoveExpired { expiry_duration: _ } => false,
+            Request::LowestOffset => true,
+            Request::HighestOffset => true,
             Request::Append { record_bytes: _ } => false,
             Request::Read { offset: _ } => true,
         }
     }
 }
 
+pub type Record = crate::commit_log::Record<'static>;
+
 pub enum Response {
-    Append {
-        write_offset: u64,
-    },
-    Read {
-        record: crate::commit_log::Record<'static>,
-        next_record_offset: u64,
-    },
+    ExpiredRemoved,
+    LowestOffset(u64),
+    HighestOffset(u64),
+    Append { write_offset: u64 },
+    Read { record: Record, next_offset: u64 },
 }
+
+pub mod in_memory;
