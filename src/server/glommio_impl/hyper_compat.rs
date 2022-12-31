52957b6eb7c7a14255e2e7ddc819e59b08a34b80
Arindam Das <dasarindam.mails@gmail.com>
2022-12-31 14:04:18 +0530

    Refactored glommio_compat module members.
    - Made response body type generic
    - Minimized nested blocks

--- src/server/glommio_impl/hyper_compat.rs
+++ src/server/glommio_impl/hyper_compat.rs
@@ -2,8 +2,10 @@
 
 use crate::server::tokio_compat::TokioIO;
 use futures_lite::Future;
-use glommio::{enclose, net::TcpListener, sync::Semaphore, TaskQueueHandle};
-use hyper::{rt::Executor, server::conn::Http, service::service_fn, Body, Request, Response};
+use glommio::{net::TcpListener, sync::Semaphore, TaskQueueHandle};
+use hyper::{
+    body::HttpBody, rt::Executor, server::conn::Http, service::service_fn, Request, Response,
+};
 use std::{io, net::SocketAddr, rc::Rc};
 use tracing::{error, error_span, instrument, Instrument};
 
@@ -21,53 +23,68 @@ where
 {
     #[instrument(skip(self, f))]
     fn execute(&self, f: F) {
-        if let Err(err) = glommio::spawn_local_into(f, self.task_queue_handle).map(|x| x.detach()) {
-            error!(
-                "Error: {:?} when spawning future on queue: {:?}",
-                err, self.task_queue_handle
-            );
-        }
+        glommio::spawn_local_into(f, self.task_queue_handle)
+            .map(|task| task.detach())
+            .map_err(|spawn_error| {
+                error!(
+                    "Error: {:?} when spawning future on queue: {:?}",
+                    spawn_error, self.task_queue_handle
+                );
+            })
+            .ok();
     }
 }
 
 /// Serves HTTP requests at the given address using the given parameters. All request handling
 /// futures are spawned on the given [`TaskQueueHandle`].
-pub async fn serve_http<S, F, R, A>(
+#[instrument(skip(addr, service))]
+pub async fn serve_http<S, RespBd, F, FError, A>(
     addr: A,
     service: S,
     max_connections: usize,
     task_queue_handle: TaskQueueHandle,
 ) -> io::Result<()>
 where
-    S: FnMut(Request<Body>) -> F + 'static + Copy,
-    F: Future<Output = Result<Response<Body>, R>> + 'static,
-    R: std::error::Error + 'static + Send + Sync,
+    S: FnMut(Request<hyper::Body>) -> F + 'static + Copy,
+    RespBd: HttpBody + 'static,
+    RespBd::Error: std::error::Error + Send + Sync,
+    F: Future<Output = Result<Response<RespBd>, FError>> + 'static,
+    FError: std::error::Error + 'static + Send + Sync,
     A: Into<SocketAddr>,
 {
     let listener = TcpListener::bind(addr.into())?;
     let conn_control = Rc::new(Semaphore::new(max_connections as _));
-    loop {
-        match listener.accept().await {
-            Err(x) => {
-                return Err(x.into());
-            }
-            Ok(stream) => {
-                let addr = stream.local_addr().unwrap();
-                glommio::spawn_local(
-                    enclose! {(conn_control) async move {
-                        let _permit = conn_control.acquire_permit(1).await;
 
-                        if let Err(x) = Http::new().with_executor(HyperExecutor{task_queue_handle})
-                            .serve_connection(TokioIO(stream), service_fn(service)).await {
-                            if !x.is_incomplete_message() {
-                                error!("Stream from {:?} failed with error {:?}", addr, x);
-                            }
-                        }
-                    }}
-                    .instrument(error_span!("hyper_http_server")),
-                )
-                .detach();
+    loop {
+        let stream = listener.accept().await?;
+        let addr = stream.local_addr()?;
+
+        let scoped_conn_control = conn_control.clone();
+
+        glommio::spawn_local_into(
+            async move {
+                let _permit = scoped_conn_control.acquire_permit(1).await;
+
+                let http = Http::new().with_executor(HyperExecutor { task_queue_handle });
+                let http = http.serve_connection(TokioIO(stream), service_fn(service));
+
+                match http.await {
+                    Err(err) if !err.is_incomplete_message() => {
+                        error!("Stream from {:?} failed with error {:?}", addr, err)
+                    }
+                    _ => (),
+                }
             }
-        }
+            .instrument(error_span!("rpc-server-tq")),
+            task_queue_handle,
+        )
+        .map(|task| task.detach())
+        .map_err(|spawn_error| {
+            error!(
+                "Error: {:?} spawning task on queue: {:?}",
+                spawn_error, task_queue_handle
+            )
+        })
+        .ok();
     }
 }
