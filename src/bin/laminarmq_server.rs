895946742f38663d55955bcfbaf4b5d640badea3
Arindam Das <dasarindam.mails@gmail.com>
2023-01-04 20:19:29 +0530

    Added tracing subscriber, respond with 404 for unmatched request URIs

--- src/bin/laminarmq_server.rs
+++ src/bin/laminarmq_server.rs
@@ -1,9 +1,12 @@
 use glommio::{executor, Latency, LocalExecutorBuilder, Placement, Shares};
 use hyper::{Body, Request, Response, StatusCode};
 use std::{convert::Infallible, rc::Rc, time::Duration};
+use tracing::{info, instrument, subscriber, Level};
+use tracing_subscriber::FmtSubscriber;
 
 use laminarmq::server::glommio_impl::hyper_compat::serve_http;
 use laminarmq::server::router::{single_node::Router, Router as _};
+
 const THREAD_NAME: &str = "laminarmq_server_thread_0";
 
 struct State {
@@ -11,20 +14,32 @@ struct State {
     pub _task_tx: (),
 }
 
+#[instrument(skip(shared_state))]
 async fn request_handler(
     shared_state: Rc<State>,
     request: Request<Body>,
 ) -> Result<Response<Body>, Infallible> {
-    let request = shared_state.router.route(request).await;
+    let response = if let Some(request) = shared_state.router.route(request).await {
+        info!("serving => {:?}", request);
+        Response::new(Body::from(format!("{:?}\n", request)))
+    } else {
+        Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from("not found\n"))
+            .expect("unable to construct body.")
+    };
 
-    Ok(Response::builder()
-        .status(StatusCode::OK)
-        .body(Body::from(format!("{:?}\n", request)))
-        .expect("unable to construct body."))
+    Ok(response)
 }
 
 #[cfg(target_os = "linux")]
 fn main() {
+    let fmt_subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+
+    subscriber::set_global_default(fmt_subscriber).expect("setting default subscriber failed");
+
     LocalExecutorBuilder::new(Placement::Unbound)
         .name(THREAD_NAME)
         .spawn(|| async move {
@@ -46,6 +61,8 @@ fn main() {
                 rpc_server_tq,
             )
             .expect("serve_http errored out.");
+
+            info!("Listening for HTTP requests on 0.0.0.0:8080");
 
             std::future::pending::<()>().await;
         })
