24ddf7f0fa702017459717e7633267ee42b538bb
Arindam Das <dasarindam.mails@gmail.com>
2023-02-15 22:23:23 +0530

    Added constructors for Index.

--- src/storage/commit_log/segmented_log/index.rs
+++ src/storage/commit_log/segmented_log/index.rs
@@ -46,10 +46,28 @@ impl IndexRecord {
     }
 }
 
+#[derive(Debug)]
 pub enum IndexError<StorageError> {
     StorageError(StorageError),
     IoError(std::io::Error),
     IncompatibleSizeType,
+    IncompatibleIdxType,
+    NoBaseIndexFound,
+    BaseIndexMismatch,
+}
+
+impl<StorageError> std::fmt::Display for IndexError<StorageError>
+where
+    StorageError: std::error::Error,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl<StorageError> std::error::Error for IndexError<StorageError> where
+    StorageError: std::error::Error
+{
 }
 
 impl IndexRecord {
@@ -118,4 +136,57 @@ where
     }
 
     Ok((index_records.first().map(|x| x.index), index_records))
+}
+
+pub struct Index<S, Idx> {
+    _index_records: Vec<IndexRecord>,
+    _base_index: Idx,
+    _storage: S,
+}
+
+#[doc(hidden)]
+macro_rules! u64_as_idx {
+    ($value:ident, $IdxType:ty) => {
+        <$IdxType as FromPrimitive>::from_u64($value).ok_or(IndexError::IncompatibleIdxType)
+    };
+}
+
+impl<S, Idx> Index<S, Idx>
+where
+    S: Storage,
+    Idx: FromPrimitive + Eq,
+{
+    async fn with_storage_and_base_index_option(
+        storage: S,
+        base_index: Option<Idx>,
+    ) -> Result<Self, IndexError<S::Error>> {
+        let (read_base_index, index_records) = index_records_in_storage(&storage).await?;
+
+        let base_index = match (read_base_index, base_index) {
+            (None, None) => Err(IndexError::NoBaseIndexFound),
+            (None, Some(base_index)) => Ok(base_index),
+            (Some(base_index), None) => u64_as_idx!(base_index, Idx),
+            (Some(read), Some(provided)) if u64_as_idx!(read, Idx)? != provided => {
+                Err(IndexError::BaseIndexMismatch)
+            }
+            (Some(_), Some(provided)) => Ok(provided),
+        }?;
+
+        Ok(Self {
+            _index_records: index_records,
+            _base_index: base_index,
+            _storage: storage,
+        })
+    }
+
+    pub async fn with_storage_and_base_index(
+        storage: S,
+        base_index: Idx,
+    ) -> Result<Self, IndexError<S::Error>> {
+        Self::with_storage_and_base_index_option(storage, Some(base_index)).await
+    }
+
+    pub async fn with_storage(storage: S) -> Result<Self, IndexError<S::Error>> {
+        Self::with_storage_and_base_index_option(storage, None).await
+    }
 }
