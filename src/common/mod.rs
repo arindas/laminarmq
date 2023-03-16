fccf9f26a165adabf831c45f87a3d833399a27f0
Arindam Das <dasarindam.mails@gmail.com>
2023-03-16 17:11:11 +0530

    feat(common): implemented wrappers for wrapping a Deref instance all the way to a HttpBody instance.

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -16,6 +16,127 @@ pub mod stream {
     impl std::error::Error for StreamBroken {}
 }
 
+pub mod ref_ops {
+
+    pub struct DerefToAsRef<T>(T);
+
+    impl<T> DerefToAsRef<T> {
+        pub fn new(deref_value: T) -> Self {
+            Self(deref_value)
+        }
+    }
+
+    impl<T, X> AsRef<[X]> for DerefToAsRef<T>
+    where
+        T: std::ops::Deref<Target = [X]>,
+    {
+        fn as_ref(&self) -> &[X] {
+            self.0.deref().as_ref()
+        }
+    }
+}
+
+pub mod http {
+    use super::ref_ops::DerefToAsRef;
+    use bytes::Buf;
+    use hyper::{
+        body::{HttpBody, SizeHint},
+        HeaderMap,
+    };
+    use std::{
+        convert::Infallible,
+        io::Cursor,
+        ops::Deref,
+        pin::Pin,
+        task::{Context, Poll},
+    };
+
+    pub struct BufToHttpBody<T>(Option<T>);
+
+    impl<T> BufToHttpBody<T> {
+        pub fn new(buf: T) -> Self {
+            Self(Some(buf))
+        }
+    }
+
+    impl<T> HttpBody for BufToHttpBody<T>
+    where
+        T: Buf + Unpin,
+    {
+        type Data = T;
+
+        type Error = Infallible;
+
+        fn poll_data(
+            mut self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
+            Poll::Ready(self.0.take().map(|x| Ok(x)))
+        }
+
+        fn poll_trailers(
+            self: Pin<&mut Self>,
+            _cx: &mut Context<'_>,
+        ) -> Poll<Result<Option<HeaderMap>, Self::Error>> {
+            Poll::Ready(Ok(None))
+        }
+
+        fn is_end_stream(&self) -> bool {
+            self.0.is_none()
+        }
+
+        fn size_hint(&self) -> SizeHint {
+            SizeHint::with_exact(
+                self.0
+                    .as_ref()
+                    .map(|body_buf| body_buf.remaining())
+                    .unwrap_or(0) as u64,
+            )
+        }
+    }
+
+    impl<T> BufToHttpBody<Cursor<DerefToAsRef<T>>>
+    where
+        T: Deref<Target = [u8]>,
+    {
+        pub fn with_deref(deref_value: T) -> Self {
+            BufToHttpBody::new(Cursor::new(DerefToAsRef::new(deref_value)))
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::BufToHttpBody;
+        use hyper::body::to_bytes;
+        use std::ops::Deref;
+
+        struct A;
+
+        const VALUE: &[u8] = b"Hello World from common::http!";
+
+        impl Deref for A {
+            type Target = [u8];
+
+            fn deref(&self) -> &Self::Target {
+                VALUE
+            }
+        }
+
+        #[test]
+        fn test_buf_to_http_body() {
+            futures_lite::future::block_on(async {
+                assert_eq!(
+                    to_bytes(BufToHttpBody::with_deref(A))
+                        .await
+                        .unwrap()
+                        .deref(),
+                    VALUE
+                )
+            });
+        }
+    }
+}
+
 pub mod split {
     //! Module providing a splittable slice abstraction.
     use std::ops::Deref;
