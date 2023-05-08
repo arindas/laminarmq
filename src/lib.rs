cb987129c3123b5c7deb484ce6cd6348b79a763f
Arindam Das <dasarindam.mails@gmail.com>
2023-05-08 22:25:01 +0530

    chore: updates crate version across different files

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@
 //! In order to use `laminarmq` as a library, add the following to your `Cargo.toml`:
 //! ```toml
 //! [dependencies]
-//! laminarmq = "0.0.4"
+//! laminarmq = "0.0.5-rc1"
 //! ```
 //!
 //! The current implementation based on [`glommio`](https://docs.rs/glommio) runs only on linux. `glommio` requires
@@ -218,7 +218,6 @@ pub mod common;
 pub mod server;
 pub mod storage;
 
-#[cfg(not(tarpaulin_include))]
 pub mod prelude {
     //! Prelude module for [`laminarmq`](super) with common exports for convenience.
 }
